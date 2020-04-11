b82d2d991f73841cae2fc6023c29206ec6803c7c
Sterling Etienne <sterlingetienne@Sterlings-MacBook-Air.local>
2020-04-10 23:24:16 -0400

    function works

--- instructions/assets/seed.sql
+++ instructions/assets/seed.sql
@@ -1,14 +1,30 @@
 -- Drops the programming_db if it already exists --
-DROP DATABASE IF EXISTS playlist_db;
+DROP DATABASE IF EXISTS seed_db;
 -- Create a database called programming_db --
-CREATE DATABASE playlist_db;
+CREATE DATABASE seed_db;
 
-use playlist_db;
+use seed_db;
 
-CREATE TABLE SONGS (
+CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL,
-    artist VARCHAR(30) NOT NULL,
-    title VARCHAR(60) NOT NULL,
-    genre VARCHAR(40) NOT NULL,
+    names VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL (5,3) NULL,
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    Last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
+
--- instructions/seed.js
+++ instructions/seed.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+const inquirer = require("inquirer")
 
 
 var connection = mysql.createConnection({
@@ -12,7 +13,7 @@ var connection = mysql.createConnection({
 
     // Your password
     password: "rootroot",
-    database: "top_songsDB"
+    database: "seed_db"
 });
 
 connection.connect(function (err) {
@@ -20,3 +21,74 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
     afterConnection();
 });
+// after connections ask user what whould they like to do with choices of options 
+function afterConnection() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "selection",
+            message: "what would you like to do today?",
+            choices: ["add_department", "add_role", "add_employee", "view_department", "view_role", "view_employee", "update_employee_role", "EXIT"]
+        }
+    ]).then(function (answers) {
+        switch (answers.selection) {
+            case "add_department":
+                addDepartment();
+                break;
+
+            case "add_role":
+                addRole();
+                break;
+
+            case "add_employee":
+                addEmployee();
+                break;
+
+            case "view_department":
+                viewDepartment();
+                break;
+
+            case "view_role":
+                viewRole();
+                break;
+
+            case "view_employee":
+                viewEmployee();
+                break;
+
+            case "update_employee_role":
+                updateEmployee();
+                break;
+
+            default: connection.end();
+                break;
+        }
+
+    })
+}
+function addDepartment() {
+    console.log("yea")
+};
+function addRole(){
+    console.log("roleee")
+};
+function addEmployee(){
+    console.log("employee")
+};
+function viewDepartment(){
+    console.log("viewdepartment")
+
+};
+function viewRole(){
+    console.log("viewrole")
+};
+
+function viewEmployee(){
+    console.log("viewdemployee")
+
+};
+
+function updateEmployee(){
+    console.log("viewdemployee")
+
+};
